4d1067bcd811f79301ca25c36db76c9e4c139d05
Philip Greenwald <philip@avidest.com>
2015-05-19 21:25:06 -0400

    Created form.  form is working

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,19 @@
 class QuestionsController < ApplicationController
 	
 	def new
-		@krash = "a house"
+		@question = Question.new
 	end
 
+	def create
+		@question = Question.new(question_params)
+		@question.save
+		render 'new'
+	end
+
+	private
+
+	def question_params
+		return params.require('question').permit('content')
+	end
 	
 end
